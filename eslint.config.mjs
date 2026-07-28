5f43f1993482816f51feedbfb2f4bb41b9787114
Bonini Alexandru <boninialexandru@gmail.com>
2026-07-29 00:30:16 +0200

    Clean up project configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,8 @@ import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const withoutLegacyReactRules = (configs) =>
-  configs.map((config) => {
-    if (!config.rules) return config;
-
-    return {
-      ...config,
-      rules: Object.fromEntries(
-        Object.entries(config.rules).filter(([rule]) => !rule.startsWith("react/"))
-      ),
-    };
-  });
-
 const eslintConfig = defineConfig([
-  ...withoutLegacyReactRules(nextVitals),
+  ...nextVitals,
   ...nextTs,
 ]);
 
